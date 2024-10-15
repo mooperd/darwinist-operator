c42201909d593b60b57cb439752b3ea6b61ffff0
Andrew Holway <andrew.holway@gmail.com>
2024-10-15 17:42:45 +0100

    more repo refector

--- script.sh
+++
@@ -1,8 +0,0 @@
-
-kubectl apply -f imageprocessingjob_crd.yaml
-kubectl apply -f operator_rbac.yaml
-kubectl apply -f operator_deployment.yaml
-kubectl apply -f api_server_rbac.yaml
-kubectl apply -f api_server_deployment.yaml
-
-kubectl port-forward svc/api-server-service 8080:8080
